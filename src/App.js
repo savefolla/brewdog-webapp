d7e2acb8df8df36321ce1692eae7686c577c065d
Saverio Follador <saverio.follador@moku.io>
2018-10-06 16:03:47 +0200

    setup router

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Homepage from "./homepage/Homepage";
+import Details from "./details/details";
+import Favorites from "./favorites/Favorites";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Router>
+        <div>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/details">Details</Link>
+            </li>
+            <li>
+              <Link to="/favorites">Favorites</Link>
+            </li>
+          </ul>
+
+          <hr />
+
+          <Route exact path="/" component={Homepage} />
+          <Route path="/details" component={Details} />
+          <Route path="/favorites" component={Favorites} />
+        </div>
+      </Router>
     );
   }
 }
