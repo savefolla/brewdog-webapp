2b594edd8a8416f23fb94f29e0ea03a842c753ec
Saverio Follador <saverio.follador@moku.io>
2018-10-06 19:14:55 +0200

    random link minor fix

--- src/Core/Header/Header.js
+++ src/Core/Header/Header.js
@@ -9,7 +9,7 @@ class Header extends Component {
       <div className="header">
         <div className="header__links">
           {this.props.location.pathname === '/' ? <div className="header__links__logo">Brewdog</div> : <NavLink exact={true} to="/">Tutte le birre</NavLink>}
-          <NavLink exact={true} to="/details">Una birra random</NavLink>
+          <NavLink exact={true} to="/details/random">Una birra random</NavLink>
           <NavLink to="/favorites">Preferiti</NavLink>
         </div>
       </div>
--- src/Details/Details.js
+++ src/Details/Details.js
@@ -1,9 +1,37 @@
 import React, { Component } from 'react';
+import Request from "request";
 
 class Details extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      beer: {},
+      busy: true
+    };
+    this.getBeer();
+  }
+  componentDidUpdate(prevProps) {
+    if (prevProps.match.params.id !== this.props.match.params.id && this.props.match.params.id === 'random') {
+      this.setState({
+        beer: {},
+        busy: false
+      });
+      this.getBeer();
+    }
+  }
+  getBeer() {
+    Request(`https://api.punkapi.com/v2/beers/${this.props.match.params.id}`, (error, response, body) => {
+      const beer = JSON.parse(body)[0];
+      this.setState({
+        beer,
+        busy: false
+      });
+      this.props.history.push(`${beer.id}`);
+    });
+  }
   render() {
     return (
-      <div>{`beer id: ${this.props.match.params.id}`}</div>
+      <div>{this.state.beer.name}</div>
     );
   }
 }
