684af4f98eb3a50fda29f4669acb8d97e1dc2b9c
Saverio Follador <saverio.follador@moku.io>
2018-10-06 18:23:37 +0200

    homepage structure

--- src/Homepage/Homepage.js
+++ src/Homepage/Homepage.js
@@ -1,27 +1,40 @@
 import React, { Component } from 'react';
 import BeerCard from "../Shared/BeerCard/BeerCard";
 import "./Homepage.scss";
+import Request from 'request';
 
 class Homepage extends Component {
+  beers = [];
+  constructor(props) {
+    super(props);
+    this.state = {
+      beers: [],
+      busy: true
+    };
+    Request('https://api.punkapi.com/v2/beers', (error, response, body) => {
+      this.setState({
+        beers: JSON.parse(body),
+        busy: false
+      });
+    });
+  }
   render() {
     return (
       <div className='homepage'>
         <div className='homepage__filters'></div>
-        <div className='homepage__table'>
+        {this.state.busy ? <div className='loading-spinner'></div> : <div className='homepage__table'>
           <div className='homepage__table__header'>
-            <div className='homepage__table__header__total'>23 birre</div>
-            <div className='homepage__table__header__paginator'>
+            <div className='homepage__table__header__total'>{this.state.beers.length} birre</div>
+            {/*<div className='homepage__table__header__paginator'>
               <div className='homepage__table__header__paginator__page homepage__table__header__paginator__page--selected'>1</div>
               <div className='homepage__table__header__paginator__page'>2</div>
               <div className='homepage__table__header__paginator__page'>3</div>
-            </div>
+            </div>*/}
           </div>
           <div className='homepage__table__list'>
-            <BeerCard starred={true} beer={{id: 1, name: 'Punk IPA 2007 - 2010', abv: 5.5}}/>
-            <BeerCard starred={false} beer={{id: 2, name: 'Punk IPA 2007 - 2010', abv: 5.5}}/>
-            <BeerCard starred={true} beer={{id: 3, name: 'Punk IPA 2007 - 2010', abv: 5.5}}/>
+            {this.state.beers.map(beer => <BeerCard key={beer.id} starred={false} beer={beer}/>)}
           </div>
-        </div>
+        </div>}
       </div>
     );
   }
