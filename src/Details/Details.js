7024bae4aad797e93c43cf75cd299affab1beb5f
Saverio Follador <saverio.follador@moku.io>
2018-10-13 12:03:45 +0200

    homepage foods and data

--- src/Details/Details.js
+++ src/Details/Details.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import Request from "request";
 import LoadingSpinner from "../Shared/LoadingSpinner/LoadingSpinner";
 
+import './Details.scss';
+
 class Details extends Component {
   constructor(props) {
     super(props);
@@ -15,7 +17,7 @@ class Details extends Component {
     if (prevProps.match.params.id !== this.props.match.params.id && this.props.match.params.id === 'random') {
       this.setState({
         beer: {},
-        busy: false
+        busy: true
       });
       this.getBeer();
     }
@@ -34,7 +36,23 @@ class Details extends Component {
   render() {
     return (
       <div className='details'>
-        {this.state.busy ? <LoadingSpinner/> : this.state.beer.name}
+        {this.state.busy ? <LoadingSpinner/> : <>
+          <div className='details__top'></div>
+          <div className='details__center'>
+            <div className='details__center__container'>
+              {this.state.beer.abv && <div>ABV: {this.state.beer.abv}%</div>}
+              {this.state.beer.srm && <div>SRM: {this.state.beer.srm}</div>}
+              {this.state.beer.ebc && <div>EBC: {this.state.beer.ebc}</div>}
+              {this.state.beer.ph && <div>PH: {this.state.beer.ph}</div>}
+            </div>
+          </div>
+          <div className='details__bottom'>
+            <div className='details__bottom__header'>Abbinamenti</div>
+            <div className='details__bottom__foods'>
+              {this.state.beer.food_pairing && this.state.beer.food_pairing.map(food => <div key={food} className='details__bottom__foods__food'>"{food}"</div>)}
+            </div>
+          </div>
+        </>}
       </div>
     );
   }
