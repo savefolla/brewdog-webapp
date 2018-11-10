2bf00d98a18b7ac79fd46ece4022b3eb7944687f
Saverio Follador <saverio@moku.io>
2018-11-10 16:41:56 +0100

    setup github pages

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ class App extends Component {
 
   render() {
     return (
-      <BrowserRouter>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
         <div className="app">
           <Header/>
           <Route exact path="/" component={Homepage} />
