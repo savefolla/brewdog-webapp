81d2d9618b5c3b317dabe12fdcda2ee01bb2fe86
Saverio Follador <saverio@moku.io>
2018-11-10 16:27:16 +0100

    Updates

---
+++ precache-manifest.2858a1da75b2040dabee927bc6fd66e2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "334eb6b53c410c5168dfff6d9211113a",
+    "url": "/brewdog-webapp/static/media/585e65d4cb11b227491c3409.334eb6b5.png"
+  },
+  {
+    "revision": "f9d21aa15f237e86d889",
+    "url": "/brewdog-webapp/static/js/runtime~main.f9d21aa1.js"
+  },
+  {
+    "revision": "538c799435917cc2edb3",
+    "url": "/brewdog-webapp/static/js/main.538c7994.chunk.js"
+  },
+  {
+    "revision": "6ccee19512e061894809",
+    "url": "/brewdog-webapp/static/js/1.6ccee195.chunk.js"
+  },
+  {
+    "revision": "538c799435917cc2edb3",
+    "url": "/brewdog-webapp/static/css/main.e41994a7.chunk.css"
+  },
+  {
+    "revision": "ab8402296146268008bd5ac26c70ec43",
+    "url": "/brewdog-webapp/index.html"
+  }
+];
